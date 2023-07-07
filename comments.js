d5c8f136a3e9077ab4b3880f54e78165a69d029f
youngilmoon <87030655+youngilmoon@users.noreply.github.com>
2023-07-07 23:56:15 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,24 @@
+// create web server
+// 1. load modules
+var http = require('http');
+
+// 2. create web server
+var server = http.createServer(function(request, response) {
+    // 3. send http header
+    // http status value: 200 : ok
+    // content type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // 4. send response body as "Hello World"
+    response.end('Hello World\n');
+}   );
+
+// 5. bind server to port 8081
+server.listen(8081);
+
+// 6. print log message
+console.log('Server running at http://localhost');
+
+// 7. run server: node comments.js
+// 8. open browser and enter url: http://localhost:8081
+// 9. output: Hello World
